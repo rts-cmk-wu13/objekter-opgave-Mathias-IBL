bad29db68b4631d14ae55a6de5e14c1156abfeef
Mathias Larsen <mathiaslarsen1122@gmail.com>
2025-02-04 09:27:20 +0100

    service section

--- js/index.js
+++ js/index.js
@@ -8,22 +8,53 @@ let newHeroSection = document.createElement("section");
  
 let heroHeadline = document.createElement("h2");
 heroHeadline.textContent = hero.headline;
-newHeroSection.appendChild(heroHeadline);
+newHeroSection.append(heroHeadline);
  
  
 let heroCopy = document.createElement("p");
 heroCopy.textContent = hero.copy;
-newHeroSection.appendChild(heroCopy);
+newHeroSection.append(heroCopy);
  
  
 let heroImage = document.createElement("img");
 heroImage.src = hero.image;
-newHeroSection.appendChild(heroImage);
+newHeroSection.append(heroImage);
  
 let heroIcon = document.createElement("img");
 heroIcon.src = hero.icon;
-newHeroSection.appendChild(heroIcon);
+newHeroSection.append(heroIcon);
  
-heroSection.appendChild(newHeroSection);
+heroSection.append(newHeroSection);
+
+
+
+
+let servicesSection = document.querySelector(".services");
+
+services.forEach(service => {
+    let newService = document.createElement("article");
+    newService.classList.add("service");
+    
+    let serviceImage = document.createElement("img");
+    serviceImage.src = service.illustration;
+    newService.append(serviceImage);
+    
+    let serviceHeadline = document.createElement("h3");
+    serviceHeadline.textContent = service.headline;
+    newService.append(serviceHeadline);
+ 
+    let serviceCopy = document.createElement("p");
+    serviceCopy.textContent = service.text;
+    newService.append(serviceCopy);
+
+    let serviceLink = document.createElement("a");
+    serviceLink.textContent = service.linktext;
+    serviceLink.href = "#";
+    newService.append(serviceLink);
+
+ 
+ 
+    servicesSection.append(newService);
+});
  
  
